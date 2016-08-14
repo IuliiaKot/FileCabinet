bb8719f2799c9cb52ec957074a04f3dd91be25e7
IuliiaKot <kotlenko.julia@gmail.com>
2016-08-14 14:32:00 -0400

    Doc controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,40 @@
+class DocsController < ApplicationController
+
+  def index
+
+  end
+
+
+  def show
+    
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+    def find_dog
+
+    end
+
+
+    def dog_params
+    end
+end
