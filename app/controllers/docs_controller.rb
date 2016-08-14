e16797186bdbbc4e9a7ec5a355e1896d1d22b3fd
IuliiaKot <kotlenko.julia@gmail.com>
2016-08-14 15:29:30 -0400

    change index showing only if user create doc

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -3,6 +3,7 @@ class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
+    @docs = current_user.docs
     @docs = Doc.all.order('created_at DESC')
   end
 
