5b6c76cbe073e8dcfbf6d21409d699120933d63e
IuliiaKot <kotlenko.julia@gmail.com>
2017-03-27 21:17:29 -0400

    test for invalid user

--- spec/models/doc_spec.rb
+++ spec/models/doc_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 describe User do
-  it 'is invalid without a email'
+
+  it 'is invalid without a email' do
+    user = User.new(email: "test@gmail.com", password: "nil66666")
+    expect(user).to be_invalid
+  end
 end
